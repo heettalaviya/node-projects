3e2a0466142852d8897f174b442419f3990e87a3
heettalaviya <heettalaviya210@gmail.com>
2026-02-11 13:59:17 +0530

    Delete PR 8 -admin-panal directory

--- PR 8 -admin-panal/middleware/flashmessage.js
+++
@@ -1,8 +0,0 @@
-const flashmessage = (req, res, next) => {
-    res.locals.flash = {
-        'success': req.flash('success'),
-        'error': req.flash('error')
-    }
-    next();
-}
-module.exports = flashmessage;
